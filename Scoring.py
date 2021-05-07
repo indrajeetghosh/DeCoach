62c193d057833f9a2485739a111a6b2ed68809fb
Indrajeet <oz35975@umbc.edu>
2021-05-07 00:26:05 -0400

    Add files via upload

---
+++ Scoring.py
@@ -0,0 +1,16 @@
+from tensorflow.keras import optimizers
+from tensorflow.keras.models import Sequential
+import tensorflow.math 
+from tensorflow.keras.layers import Dense , Conv1D, LSTM, MaxPooling1D, Flatten, Dropout, BatchNormalization,AveragePooling1D
+
+#Please change the input_size dimension as per your input data and also no. of neurons in last FC layer  
+
+def model():
+    model = Sequential()
+    model.add(Conv1D(32, 2, activation="relu", input_shape=(128,12)))
+    model.add(Conv1D(64, 3, activation="relu", input_shape=(128,12)))
+    model.add(Flatten())
+    model.add(Dense(64, activation="relu"))
+    model.add(Dense(1))
+    model.compile(loss="mse", optimizer="adam")
+    return model
